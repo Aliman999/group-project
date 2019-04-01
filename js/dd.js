afabc7121e459c0ea9c8899a51ad36f8dd8f96d8
Ali <jamesdusky@dustytavern.com>
2019-04-01 14:50:10 -0400

    Began Dropdowns in navbar

---
+++ js/dd.js
@@ -0,0 +1,18 @@
+$(function() {
+
+  $(".shopd").mouseover(function(){
+    $(".shopdd").css("display","block");
+  });
+  $(".shopdd").mouseover(function(){
+    $(".shopdd").css("display","block");
+  });
+
+
+  $(".shopd").mouseout(function(){
+    $(".shopdd").css("display", "none");
+  });
+  $(".shopdd").mouseout(function(){
+    $(".shopdd").css("display","none");
+  });
+
+});
